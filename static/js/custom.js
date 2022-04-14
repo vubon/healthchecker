3795739824a86a37475a5abd6ac696e533f19ef0
vubon <vubon.roy@gmail.com>
2022-04-14 23:12:20 +0600

    Click menu selection issue solved

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,15 @@
-/* globals Chart:false, feather:false */
-(function(){
-  $('#msbo').on('click', function(){
-    $('body').toggleClass('msb-x');
-  });
-}());
+$('.nav li a').click(function (e) {
+    localStorage.setItem('active-menu-item', $(this).attr("href"));
+});
+
+$(document).ready(function () {
+    const activeMenuItem = $('.nav [href="' + localStorage.getItem('active-menu-item') + '"]').first();
+    console.log(activeMenuItem);
+    activeMenuItem && setActiveLink(activeMenuItem);
+});
+
+function setActiveLink($el) {
+    $(".nav li").removeClass("active");
+    $el.parent().addClass('active');
+    $el.click();
+}
