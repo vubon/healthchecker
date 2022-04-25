bb2593595c890349aff224486bca9a2e772a4fa1
vubon <vubon.roy@gmail.com>
2022-04-25 10:08:52 +0600

    Docker is resolved

--- start.sh
+++ start.sh
@@ -2,10 +2,26 @@
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
-python manage.py migrate
+#python manage.py migrate
 python manage.py collectstatic --no-input
-exec gunicorn healthchecker.wsgi:application \
-      --name verifier \
-      --bind 0.0.0.0:8000 \
-      --workers 3 \
-      --access-logfile - --error-logfile - --log-level debug
+
+
+echo "Flush the manage.py command it any"
+
+while ! python manage.py flush --no-input 2>&1; do
+  echo "Flusing django manage command"
+  sleep 5
+done
+
+echo "Migrate the Database at startup of project"
+
+# Wait for few minute and run db migraiton
+while ! python manage.py migrate  2>&1; do
+   echo "Migration is in progress status"
+   sleep 5
+done
+
+python manage.py shell < config/create_user.py
+echo "Django docker is fully configured successfully."
+
+exec gunicorn healthchecker.wsgi:application -c ./config/gunicorn.config.py
